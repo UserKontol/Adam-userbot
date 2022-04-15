2696e2ac84f64338666428d553d7cfaa05b7c7e9
UserKontol <101336638+UserKontol@users.noreply.github.com>
2022-04-15 16:37:27 +0000

    Update button.py

--- userbot/modules/button.py
+++ userbot/modules/button.py
@@ -51,7 +51,7 @@ CMD_HELP.update(
         "button": f"**Plugin : **`button`\
         \n\n  •  **Syntax :** `{cmd}button` <text> [Name on button]<buttonurl:link you want to open>\
         \n  •  **Function : **Untuk membuat pesan button melalui inline\
-        \n  •  **Examples : **`{cmd}button test [google]<buttonurl:https://www.google.com> [Channel]<buttonurl:https://t.me/ProjectJoni:same> [Support]<buttonurl:https://t.me/JoniSupport>`\
+        \n  •  **Examples : **`{cmd}button test [google]<buttonurl:https://www.google.com> [Channel]<buttonurl:https://t.me/adamcodeubot:same> [Support]<buttonurl:https://t.me/wibu_telegram>`\
     "
     }
 )
